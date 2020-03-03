fff7b701a9e77e2db7e01f18b1d68cea2822eb39
Eric Luc <ericluc@Erics-MacBook-Pro.local>
2020-03-02 21:00:38 -0500

    Subject: Finished the Plugin exercise

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const webpack = require('webpack')
+const HtmlWebPackPlugin = require('html-webpack-plugin')
 module.exports ={
     entry: './src/client/index.js',
     module:{
@@ -12,7 +13,13 @@ module.exports ={
 
 
 	]
-	}
+	},
+	plugins: [
+		new HtmlWebPackPlugin({
+			template: "./src/client/views/index.html",
+			filename: "./index.html"
+		})
+	]
 
 
 }
